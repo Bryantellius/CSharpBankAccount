113cf19815daa9349e0e38c70658395fda02bbb6
Ben Bryant <brbryant2639@gmail.com>
2020-08-01 22:58:12 -0500

    Quick walkthrough of microsoft docs creating a bankaccount with classes

---
+++ OOPClassesCSharp/BankAccount.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+namespace OOPClassesCSharp
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { 
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+
+        private static int accountNumber = 1234567890;
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Yo dawg, deposit gotta be positive.");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+        public void MakeWithdrawel(decimal amount, DateTime date, string note)
+        {
+            if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Yo dawg, withdrawel gotta be positive");
+            }
+            if(Balance -amount < 0)
+            {
+                throw new InvalidOperationException("Yo dawg, we can't go broke!");
+            }
+            var withdrawel = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawel);
+        }
+
+        public string GetTransactions()
+        {
+            var report = new System.Text.StringBuilder();
+            decimal balance = 0;
+            report.AppendLine("Date\t\tAmount\tBalance\tNote");
+            foreach (var item in allTransactions)
+            {
+                balance += item.Amount;
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{balance}\t{item.Notes}");
+            }
+
+            return report.ToString();
+        }
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+            this.Number = accountNumber.ToString();
+            accountNumber++;
+        }
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+    }
+}
---
+++ OOPClassesCSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OOPClassesCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var account = new BankAccount("Ben Bryant", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+            Console.WriteLine();
+            account.MakeDeposit(100, DateTime.Now, "Happy Birthday");
+            account.MakeDeposit(733, DateTime.Now, "Payday");
+            account.MakeWithdrawel(600, DateTime.Now, "Rent");
+            account.MakeWithdrawel(100, DateTime.Now, "Utilities");
+
+            Console.WriteLine(account.GetTransactions());
+        }
+    }
+}
